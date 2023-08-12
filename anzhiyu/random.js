43dee2721f77fbe5763b9c5980329f81e070de4e
kouwei <kouwei6986@qq.com>
2023-08-12 22:58:08 +0800

    Site updated: 2023-08-12 22:58:07

---
+++ anzhiyu/random.js
@@ -0,0 +1,1 @@
+var posts=["posts/9520.html","posts/9521.html","posts/79666db.html","posts/dbeddcc1.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
